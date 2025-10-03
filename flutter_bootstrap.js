77a17ebce359c252ee7410ca434098949e7cd7a2
William-Rincon <90631331+William-Rincon@users.noreply.github.com>
2025-10-02 22:42:14 -0500

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2720788921"
+    serviceWorkerVersion: "3286948362"
   }
 });
