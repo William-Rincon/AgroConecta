cdcbddd0d0768787075354cc230b1cb21a841d4b
William-Rincon <90631331+William-Rincon@users.noreply.github.com>
2025-10-03 22:08:04 -0500

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,14 +22,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "297dbb55eda44c406429d0d9d366a236",
+"flutter_bootstrap.js": "fe11f6443c1b21da7839d94117ece209",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "cddac9f6a2a8a571e4b53e3ae9dcb845",
 "/": "cddac9f6a2a8a571e4b53e3ae9dcb845",
-"main.dart.js": "1f5c4d674b860da86f4a36948c835e36",
+"main.dart.js": "4af0d7c8129847cfdaeb53fe0c83a010",
 "manifest.json": "3680f7e86590be8407f549a460fcf537",
 "version.json": "90979a06ae4bbd42cee44d0f3175c5f9"};
 // The application shell files that are downloaded before a service worker can
